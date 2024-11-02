10ecfe8ca381e7fb3e70e4c55eba67420ff9d21d
theshaikhina <shaikhinasandu@gmail.com>
2024-11-02 18:16:23 +0300

    Add server setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+// server/server.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
